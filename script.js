4711da4a657655597dda7cbd43b77b6f24421260
Yahya <y.elmurzaevv@gmail.com>
2023-09-25 17:26:41 +0300

    add functions - start, rememberMyFilms, detectedPersonalLevel, writeYourGenres.

--- script.js
+++ script.js
@@ -547,24 +547,237 @@
 // const perem = 'Hello world';
 // console.log(perem.slice(6,11))
 
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false
+// };
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+// const a = prompt('Один из последних просмотренных фильмов?',''),
+//       b = prompt('На сколько оцените его?',''),
+//       c = prompt('Один из последних просмотренных фильмов?',''),
+//       d = prompt('На сколько оцените его?','');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+//Условия Условия Условия Условия Условия Условия Условия Условия
+
+// if (4) {
+//     console.log('Верно')
+// }else {
+//     console.log('Неверно')
+// }
+
+// if (num < 50) {
+//     console.log('Мало')
+// }else if (num > 50){
+//     console.log('Много')
+// }else{
+//     console.log('Ровно 50')
+// }
+
+// (num === 50) ? console.log('Верно') : console.log('Неверно'); //Тернарный оператор
+
+// const num = 200;
+// switch (num) {
+//     case 49:
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Тоже неверно');
+//         break;
+//     case 50:
+//         console.log('Верно');
+//         break;
+//     default:
+//         console.log('Не сегодня');
+//         break;
+
+// }
+
+//Циклы Циклы Циклы Циклы Циклы Циклы Циклы Циклы Циклы Циклы Циклы
+
+//while - пока
+//do - делать
+//for - для
+// while (num <= 55) {
+//     console.log(num)
+//     num++
+// }
+
+// do {
+//     console.log(num)
+//     num++
+// }
+// while (num <= 55)
+
+// let num = 50;
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(num);
+//     num++
+// }
+
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
+
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt("Один из последних просмотренных фильмов?", ""),
+//         b = prompt("На сколько оцените его?", "");
+
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log('done');
+//   }else {
+//     i--;
+//     console.log('error')
+//   }
+
+// }
+
+// if (personalMovieDB.count < 10) {
+//     console.log('Просмотрено довольно мало фильмов');
+// }else if (personalMovieDB.count >= 10 && personalMovieDB.count >= 30) {
+//     console.log('Вы классический зритель');
+// }else if (personalMovieDB.count > 30) {
+//     console.log('Вы киноман');
+// }else {
+//     console.log('Походу вы не ответили на вопросы :(')
+// }
+
+// console.log(personalMovieDB);
+
+//Функции
+// let num  = 20;
+// function showFirstMessage(text) {
+//     console.log(text);
+//     console.log(num);
+// }
+// showFirstMessage('Hello World');
+// console.log(num);
+
+// console.log(calc(4, 6));
+// console.log(calc(5, 25));
+// console.log(calc(8, 12));
+
+// function calc(a,b) {
+//     return(a+b);
+// }
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+//Function Expression
+// const logger = function() {
+//     console.log('Hello');
+// };
+// logger();
+
+//Стрелочные функции
+// const calc = (a, b) => {return a + b};
+// console.log(calc(33, 7));
+
+// const str = 'teSt';
+// const arr = [1, 2, 4]
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str[2]);
+// const fruit = 'Some fruit';
+// console.log(fruit.indexOf('fruit'))
+
+// const logg = 'Hello world';
+// console.log(logg.slice(6));
+// console.log(logg.substring(6, 11));
+// console.log(logg.substr(6, 5));
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+// const test = '12.2px';
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
+
+//Практика ч.1
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
-  privat: false
+  privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько оцените его?','');
+//Практика ч.2
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
+    if (a != "" && b != "" && a != null && b != null && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log("Верно");
+    } else {
+      console.log("Неверно");
+      i--;
+    }
+  }
+}
+// rememberMyFilms();
 
-console.log(personalMovieDB);
+function detectedPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log("Вы классический зритель");
+  } else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+  } else {
+    console.log("Произошла ошибка!");
+  }
+}
+
+// detectedPersonalLevel();
+
+
+function showMyDB(hidden) {
+  if(!hidden) {
+  console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for(let i = 1; i <= 3; i++) { 
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+  }
+
+}
+writeYourGenres();
